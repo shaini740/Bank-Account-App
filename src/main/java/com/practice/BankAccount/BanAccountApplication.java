87e9cadb6408ca3944be544bd3d3a890054d3581
shaini740 <shainis378@gmail.com>
2025-03-18 19:46:58 +0530

    Created a new Spring Application project - Bank Account

---
+++ src/main/java/com/practice/BankAccount/BanAccountApplication.java
@@ -0,0 +1,13 @@
+package com.practice.BankAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/BankAccount/BanAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.BankAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
